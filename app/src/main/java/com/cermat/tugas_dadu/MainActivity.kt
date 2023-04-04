49c39c42c956b4a6b3601287ef2ffb4d9ba2ffae
martin <ilhammartin2@gmail.com>
2023-04-04 22:26:39 +0700

    Projek_dadu

---
+++ app/src/main/java/com/cermat/tugas_dadu/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.cermat.tugas_dadu
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+}
+
+
+
+
+
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+//        return (1..numSides).random()
+        return Random().nextInt(numSides) + 1
+    }
+}
+
+
+
+
+
